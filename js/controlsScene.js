3d3d216a218cf770359f957747d0ff091b761663
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-06-08 12:27:55 -0400

    fixed formating with prettier

--- js/controlsScene.js
+++ js/controlsScene.js
@@ -7,31 +7,30 @@
 /**
  * This class is the ControlsScene Scene.
  */
- class ControlsScene extends Phaser.Scene {
+class ControlsScene extends Phaser.Scene {
+  /**
+   * This method is the construtor.
+   */
+  constructor() {
+    super({ key: "controlsScene" });
+
+    this.controlsSceneBackgroundImage = null;
+    this.okButton = null;
+    this.controls = null;
+    this.controlsSceneText = null;
+    this.controlsSceneTextStyle = {
+      font: "60px Times",
+      fill: "#ffffff",
+      align: "center",
+    };
+
     /**
-     * This method is the construtor.
-     */
-    constructor() {
-      super({ key: "controlsScene" })
-  
-      this.controlsSceneBackgroundImage = null
-      this.okButton = null
-      this.controls = null
-      this.controlsSceneText = null
-      this.controlsSceneTextStyle = {
-        font: "60px Times",
-        fill: "#ffffff",
-        align: "center",
-      }
-  
-    /**  
      * Can be defined on your own Scenes.
      * This method is called by the Scene Manager when the scene starts,
      *   before preload() and create().
      * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
      */
-     
-}
+  }
 
   /**
    * Can be defined on your own Scenes.
@@ -40,7 +39,7 @@
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff")
+    this.cameras.main.setBackgroundColor("#ffffff");
   }
 
   /**
@@ -48,10 +47,10 @@
    * Use it to load assets.
    */
   preload() {
-    console.log("Controls Scene")
-    this.load.image("controlsSceneBackground", "./assets/game_backdrop.png")
-    this.load.image("okbutton", "./assets/ok_button.png")
-    this.load.image("controls", "./assets/controls.gif")
+    console.log("Controls Scene");
+    this.load.image("controlsSceneBackground", "./assets/game_backdrop.png");
+    this.load.image("okbutton", "./assets/ok_button.png");
+    this.load.image("controls", "./assets/controls.gif");
   }
 
   /**
@@ -64,30 +63,29 @@
       0,
       0,
       "controlsSceneBackground"
-    )
-    this.controlsSceneBackgroundImage.x = 1080 / 2
-    this.controlsSceneBackgroundImage.y = 1080 / 2
+    );
+    this.controlsSceneBackgroundImage.x = 1080 / 2;
+    this.controlsSceneBackgroundImage.y = 1080 / 2;
 
-    this.okButton = this.add.sprite(1080 / 2, 1620 / 2 + 100, "okbutton")
-    this.okButton.setInteractive({ useHandCursor: true })
-    this.okButton.on("pointerdown", () => this.clickButton())
+    this.okButton = this.add.sprite(1080 / 2, 1620 / 2 + 100, "okbutton");
+    this.okButton.setInteractive({ useHandCursor: true });
+    this.okButton.on("pointerdown", () => this.clickButton());
 
     this.controls = this.add
       .sprite(1080 / 2, 440 / 2 + 100, "controls")
-      .setScale(1.5)
+      .setScale(1.5);
 
     this.controlsSceneText = this.add.text(
       1080 / 2,
       1280 / 2,
       "Use arrow keys or WASD to move",
       this.controlsSceneTextStyle
-    )
-    clickButton()
+    );
+    clickButton();
     {
-      this.scene.start("menuScene")
+      this.scene.start("menuScene");
     }
   }
- }
+}
 
-  export default ControlsScene
-  
+export default ControlsScene;
--- js/game.js
+++ js/game.js
@@ -4,12 +4,12 @@
 // Created on: June 2022
 // This is the Phaser3 configuration file
 
-import ControlsScene from "./controlsScene.js"
-import MenuScene from "./menuScene.js"
+import ControlsScene from "./controlsScene.js";
+import MenuScene from "./menuScene.js";
 
 // Our game scenes
-const controlsScene = new ControlsScene()
-const menuScene = new MenuScene()
+const controlsScene = new ControlsScene();
+const menuScene = new MenuScene();
 
 /** Game scene.*/
 const config = {
@@ -29,15 +29,15 @@ const config = {
     // we place it in the middle of the page.
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-}
+};
 
-const game = new Phaser.Game(config)
-console.log(game)
+const game = new Phaser.Game(config);
+console.log(game);
 
 // load scenes
 // Note: remember any "key" is global and CAN NOT be reused!
-game.scene.add("controlsScene", controlsScene)
-game.scene.add("menuScene", menuScene)
+game.scene.add("controlsScene", controlsScene);
+game.scene.add("menuScene", menuScene);
 
 // the start scene
-game.scene.start("controlsScene")
+game.scene.start("controlsScene");
