4fa9555a5312ad9e0e068d25b4071c59ba2c0704
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-06-08 20:40:26 -0400

    removed semi colons

--- js/gameSceneHard.js
+++ js/gameSceneHard.js
@@ -11,10 +11,10 @@ class GameSceneHard extends Phaser.Scene {
    * This method is the construtor.
    */
   constructor() {
-    super({ key: "gameSceneHard" });
+    super({ key: "gameSceneHard" })
 
-    this.gameSceneHardBackgroundImage = null;
-    this.monkeyHard = null;
+    this.gameSceneHardBackgroundImage = null
+    this.monkeyHard = null
   }
 
   /**
@@ -24,7 +24,7 @@ class GameSceneHard extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff");
+    this.cameras.main.setBackgroundColor("#ffffff")
   }
 
   /**
@@ -32,12 +32,12 @@ class GameSceneHard extends Phaser.Scene {
    * Use it to load assets.
    */
   preload() {
-    console.log("Game Scene Hard");
+    console.log("Game Scene Hard")
     this.load.image(
       "gameSceneHardBackground",
       "./assets/hard_game_backdrop.png"
-    );
-    this.load.image("monkey", "./assets/monkey.gif");
+    )
+    this.load.image("monkey", "./assets/monkey.gif")
   }
 
   /**
@@ -50,31 +50,31 @@ class GameSceneHard extends Phaser.Scene {
       0,
       0,
       "gameSceneHardBackground"
-    );
-    this.gameSceneHardBackgroundImage.x = 1080 / 2;
-    this.gameSceneHardBackgroundImage.y = 1080 / 2;
+    )
+    this.gameSceneHardBackgroundImage.x = 1080 / 2
+    this.gameSceneHardBackgroundImage.y = 1080 / 2
 
     this.monkeyHard = this.physics.add
       .sprite(1080 / 2, 1080 / 2, "monkey")
-      .setScale(0.35);
+      .setScale(0.35)
   }
 
   update(time, delta) {
     //called 60 times a second.
 
-    const keyForwardObj = this.input.keyboard.addKey("W");
-    const keyLeftObj = this.input.keyboard.addKey("A");
-    const keyBackwardObj = this.input.keyboard.addKey("S");
-    const keyRightObj = this.input.keyboard.addKey("D");
-    const keyForwardObjAlt = this.input.keyboard.addKey("UP");
-    const keyLeftObjAlt = this.input.keyboard.addKey("LEFT");
-    const keyBackwardObjAlt = this.input.keyboard.addKey("DOWN");
-    const keyRightObjAlt = this.input.keyboard.addKey("RIGHT");
+    const keyForwardObj = this.input.keyboard.addKey("W")
+    const keyLeftObj = this.input.keyboard.addKey("A")
+    const keyBackwardObj = this.input.keyboard.addKey("S")
+    const keyRightObj = this.input.keyboard.addKey("D")
+    const keyForwardObjAlt = this.input.keyboard.addKey("UP")
+    const keyLeftObjAlt = this.input.keyboard.addKey("LEFT")
+    const keyBackwardObjAlt = this.input.keyboard.addKey("DOWN")
+    const keyRightObjAlt = this.input.keyboard.addKey("RIGHT")
 
     if (keyForwardObj.isDown || keyForwardObjAlt.isDown) {
-      this.monkeyHard.y -= 10;
+      this.monkeyHard.y -= 10
       if (this.monkeyHard.y < 100) {
-        this.monkeyHard.y += 10;
+        this.monkeyHard.y += 10
       }
       if (
         this.monkeyHard.x < 250 ||
@@ -82,14 +82,14 @@ class GameSceneHard extends Phaser.Scene {
         (this.monkeyHard.x > 290 && this.monkeyHard.x < 530) ||
         (this.monkeyHard.x < 780 && this.monkeyHard.x > 550)
       ) {
-        this.monkeyHard.y += 10;
+        this.monkeyHard.y += 10
       }
     }
 
     if (keyLeftObj.isDown || keyLeftObjAlt.isDown) {
-      this.monkeyHard.x -= 10;
+      this.monkeyHard.x -= 10
       if (this.monkeyHard.x < 100) {
-        this.monkeyHard.x += 10;
+        this.monkeyHard.x += 10
       }
       if (
         this.monkeyHard.y < 250 ||
@@ -97,14 +97,14 @@ class GameSceneHard extends Phaser.Scene {
         (this.monkeyHard.y > 290 && this.monkeyHard.y < 530) ||
         (this.monkeyHard.y < 780 && this.monkeyHard.y > 550)
       ) {
-        this.monkeyHard.x += 10;
+        this.monkeyHard.x += 10
       }
     }
 
     if (keyBackwardObj.isDown || keyBackwardObjAlt.isDown) {
-      this.monkeyHard.y += 10;
+      this.monkeyHard.y += 10
       if (this.monkeyHard.y > 980) {
-        this.monkeyHard.y -= 10;
+        this.monkeyHard.y -= 10
       }
       if (
         this.monkeyHard.x < 250 ||
@@ -112,14 +112,14 @@ class GameSceneHard extends Phaser.Scene {
         (this.monkeyHard.x > 290 && this.monkeyHard.x < 530) ||
         (this.monkeyHard.x < 780 && this.monkeyHard.x > 550)
       ) {
-        this.monkeyHard.y -= 10;
+        this.monkeyHard.y -= 10
       }
     }
 
     if (keyRightObj.isDown || keyRightObjAlt.isDown) {
-      this.monkeyHard.x += 10;
+      this.monkeyHard.x += 10
       if (this.monkeyHard.x > 980) {
-        this.monkeyHard.x -= 10;
+        this.monkeyHard.x -= 10
       }
       if (
         this.monkeyHard.y < 250 ||
@@ -127,10 +127,10 @@ class GameSceneHard extends Phaser.Scene {
         (this.monkeyHard.y > 290 && this.monkeyHard.y < 530) ||
         (this.monkeyHard.y < 780 && this.monkeyHard.y > 550)
       ) {
-        this.monkeyHard.x -= 10;
+        this.monkeyHard.x -= 10
       }
     }
   }
 }
 
-export default GameSceneHard;
+export default GameSceneHard
