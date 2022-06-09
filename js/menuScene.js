6de592f8b8349cfa18c8fcc1f683ffd611d556d8
Titus-diceman <99354708+Titus-diceman@users.noreply.github.com>
2022-06-09 12:20:58 -0400

    Prettyafided it

--- js/menuScene.js
+++ js/menuScene.js
@@ -6,16 +6,16 @@
 /**
  * This class is the Splash Scene.
  */
-  class MenuScene extends Phaser.Scene {
+class MenuScene extends Phaser.Scene {
   /**
    * This method is the construtor.
    */
   constructor() {
-    super({ key: "menuScene" })
+    super({ key: "menuScene" });
 
-    this.menuSceneBackgroundImage = null
-    this.startButtonNormal = null
-    this.startButtonHard = null
+    this.menuSceneBackgroundImage = null;
+    this.startButtonNormal = null;
+    this.startButtonHard = null;
   }
 
   /**
@@ -25,7 +25,7 @@
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff")
+    this.cameras.main.setBackgroundColor("#ffffff");
   }
 
   /**
@@ -33,11 +33,11 @@
    * Use it to load assets.
    */
   preload() {
-    console.log("Menu Scene")
-    this.load.image("menuSceneBackground", "assets/game_backdrop.png")
-    this.load.image("startButtonNormal", "assets/normal_start_button.png")
-    this.load.image("startButtonHard", "assets/hard_start_button.png")
-   }
+    console.log("Menu Scene");
+    this.load.image("menuSceneBackground", "assets/game_backdrop.png");
+    this.load.image("startButtonNormal", "assets/normal_start_button.png");
+    this.load.image("startButtonHard", "assets/hard_start_button.png");
+  }
 
   /**
    * Can be defined on your own Scenes.
@@ -49,18 +49,26 @@
       0,
       0,
       "menuSceneBackground"
-    )
-    this.menuSceneBackgroundImage.x = 1080 / 2
-    this.menuSceneBackgroundImage.y = 1080 / 2 
-      
-    this.startButtonNormal = this.add.sprite(1080 / 2, (540 / 2) + 100, "startButtonNormal")
-    this.startButtonNormal.setInteractive({ useHandCursor: true })
-    this.startButtonNormal.on("pointerdown", () => this.clickButtonNoraml())
+    );
+    this.menuSceneBackgroundImage.x = 1080 / 2;
+    this.menuSceneBackgroundImage.y = 1080 / 2;
 
-    this.startButtonHard = this.add.sprite(1080 / 2, (1080 / 2) + 100, "startButtonHard")
-    this.startButtonHard.setInteractive({ useHandCursor: true })
-    this.startButtonHard.on("pointerdown", () => this.clickButtonHard())
+    this.startButtonNormal = this.add.sprite(
+      1080 / 2,
+      540 / 2 + 100,
+      "startButtonNormal"
+    );
+    this.startButtonNormal.setInteractive({ useHandCursor: true });
+    this.startButtonNormal.on("pointerdown", () => this.clickButtonNoraml());
+
+    this.startButtonHard = this.add.sprite(
+      1080 / 2,
+      1080 / 2 + 100,
+      "startButtonHard"
+    );
+    this.startButtonHard.setInteractive({ useHandCursor: true });
+    this.startButtonHard.on("pointerdown", () => this.clickButtonHard());
   }
 }
 
-export default MenuScene
+export default MenuScene;
